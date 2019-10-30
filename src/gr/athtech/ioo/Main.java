0ca04aeec679b76818cfc0cf35cafb6cfc11a001
Akorovessis <akorovesis@athtech.gr>
2019-10-30 13:25:49 +0200

    Initial Commit - Only airports

--- src/gr/athtech/ioo/Main.java
+++ src/gr/athtech/ioo/Main.java
@@ -7,5 +7,7 @@ public class Main {
 	f1.setDepAirport("Athens");
 	f1.setArAirport("Montreal");
         System.out.println(f1.toString());
+        System.out.println("Departing: " + f1.getArAirport());
+        System.out.println("Arriving: " + f1.getDepAirport());
     }
 }
